edbefb37b2ec60feba3e2b597e117d6f1c8140eb
mudrutom <mudrutom@gmail.com>
2013-11-07 10:36:05 +0100

    added VIA Tracker app project

---
+++ app/VIA Tracker app/src/main/java/cz/cvut/via/tracker/app/IssueDetailFragment.java
@@ -0,0 +1,61 @@
+package cz.cvut.via.tracker.app;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import cz.cvut.via.tracker.app.dummy.DummyContent;
+
+/**
+ * A fragment representing a single Issue detail screen.
+ * This fragment is either contained in a {@link IssueListActivity}
+ * in two-pane mode (on tablets) or a {@link IssueDetailActivity}
+ * on handsets.
+ */
+public class IssueDetailFragment extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /**
+     * The dummy content this fragment is presenting.
+     */
+    private DummyContent.DummyItem mItem;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public IssueDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_issue_detail, container, false);
+
+        // Show the dummy content as text in a TextView.
+        if (mItem != null) {
+            ((TextView) rootView.findViewById(R.id.issue_detail)).setText(mItem.content);
+        }
+
+        return rootView;
+    }
+}
