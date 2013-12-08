f2de1a4ff0fd74210c65a5f3b018fff1b9c1446c
mudrutom <mudrutom@gmail.com>
2013-12-08 20:53:41 +0100

    added name strings for Issue state

--- app/VIA Tracker app/src/main/java/cz/cvut/via/tracker/app/model/IssueState.java
+++ app/VIA Tracker app/src/main/java/cz/cvut/via/tracker/app/model/IssueState.java
@@ -1,13 +1,20 @@
 package cz.cvut.via.tracker.app.model;
 
+import cz.cvut.via.tracker.app.R;
+
 public enum IssueState {
 
-	open(1), inProgress(2), fixed(3), wontFix(4);
+	open(1, R.string.issueState_open),
+	inProgress(2, R.string.issueState_inProgress),
+	fixed(3, R.string.issueState_fixed),
+	wontFix(4, R.string.issueState_wontFix);
 
-	final int num;
+	public final int num;
+	public final int nameRes;
 
-	private IssueState(int num) {
+	private IssueState(int num, int nameRes) {
 		this.num = num;
+		this.nameRes = nameRes;
 	}
 
 	public static IssueState valueOf(int num) {
